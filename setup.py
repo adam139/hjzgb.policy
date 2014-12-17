1f4b81fb5e1557f395124d7c7b23913c20165039
adam139 <yuejun.tang@gmail.com>
2014-12-17 12:51:33 +0800

    update policy

--- setup.py
+++ setup.py
@@ -27,6 +27,9 @@ setup(name='hjzgb.policy',
           'setuptools',
           # -*- Extra requirements: -*-
       ],
+      extras_require={
+    'test': ['plone.app.testing',]
+        },          
       entry_points="""
       # -*- Entry points: -*-
 
